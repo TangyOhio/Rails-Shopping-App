60fe86f4562681c26aff39df80f9a4090b025f2d
Jace <jacemcgough1@gmail.com>
2018-05-24 21:04:59 -0600

    Did the stuffs

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  belongs_to :department
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+  validates :price, presence: true
+  validates :price, numericality: { only_float: true }
+
+  def list_item
+    puts "#{self.name} - #{self.price}"
+  end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,2 +1,7 @@
 class Store < ApplicationRecord
+  has_many :departments, dependent: :destroy
+
+  validates :name, presence: true
+  
+  validates :address, uniqueness: true
 end
